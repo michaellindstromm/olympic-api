10281882f61c42ef16366d2dd48787015ad46a96
Michael Lindstrom <michaellindstr@gmail.com>
2017-12-05 20:12:00 -0600

    enabling cors

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,11 @@ module OlympicApi
     config.autoload_paths << Rails.root.join('lib')
     config.api_only = true
     config.middleware.use Rack::Attack
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
