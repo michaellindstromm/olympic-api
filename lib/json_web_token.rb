e49e6a55233d0d0c871eb67b5bd2cf06e1ee03af
Michael Lindstrom <michaellindstr@gmail.com>
2017-12-05 23:00:27 -0600

    shit man

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -30,7 +30,7 @@ class JsonWebToken
         def decode(token)
             body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0] 
             HashWithIndifferentAccess.new body 
-        rescue nil
+        rescue => error
         end 
     end 
 end
