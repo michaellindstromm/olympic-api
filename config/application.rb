12cf451343b8e8d58614fd5e1cc042a9d5198c7a
Michael Lindstrom <michaellindstr@gmail.com>
2017-12-05 22:53:31 -0600

    attempting eager load to fix problem

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module OlympicApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths += %W(#{Rails.root}/lib)
     config.api_only = true
     config.middleware.use Rack::Attack
     config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
