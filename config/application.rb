c6517f678104bb222624509aeaf2b79c1dd5f22c
Michael Lindstrom <michaellindstr@gmail.com>
2017-12-05 22:07:09 -0600

    changed middleware insert_before

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module OlympicApi
     config.autoload_paths << Rails.root.join('lib')
     config.api_only = true
     config.middleware.use Rack::Attack
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
