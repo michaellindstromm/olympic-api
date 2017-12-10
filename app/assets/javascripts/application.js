50b043e2029a348a09c6aa85b105d3f209c85819
Michael Lindstrom <michaellindstr@gmail.com>
2017-12-09 23:48:41 -0600

    working on live examples and styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,34 +20,39 @@ let listeners = function() {
     });
 
 
+    $('.dark-dropdowns a').on('click', function(e) {
+        e.preventDefault();
+        $(this).parent().siblings('.dark-dropdowns-button').text($(this).text());
+        $(this).parent().siblings('.dark-dropdowns-button').val($(this).text());
+    });
+
 };
 
-var token = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1MTI4ODAyNjN9.b27eif-hGhCEVo77AzfDA1v37f4A39u8O9TuItZTY58';
+var token = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1MTI4OTA4NjF9.vF3Pi7WX1zWcmHdCk_Qz7J3sfagJJCKwIhFLvdaXsRE';
+
+$.ajax({
+    url: '/api/olympics?page=4&per_page=15',
+    headers: {
+        'Authorization': token,
+        'Accept': 'version=v2'
+    }
+}).done(function (res) {
+    console.log(res)
+}).fail(function(e) {
+    console.log(e);
+});
 
 $.ajax({
     url: "/api/endpoints",
 }).done(function (res) {
-    makeCall(res);
+    console.log(res);
 });
-
-function makeCall(endpoints) {
-
-    $.ajax({
-        url: '/api/olympics/49?page=4&per_page=15',
-        headers: {
-            'Authorization': token,
-            'Accept': 'version=v2'
-        }
-    }).done(function (res) {
-        console.log(res)
-    });
-};
 
 // $.ajax({
 //     method: 'POST',
-//     url: '/authenticate',
+//     url: '/api/authenticate',
 //     data: {
-//         'email':'new@new.com',
+//         'email':'test@test.com',
 //         'password':'123123123'
 //     }
 // }).done(function (res) {
