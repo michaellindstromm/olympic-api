4f43854e8752b9113eca148b52af4c3ff204bce8
Michael Lindstrom <michaellindstr@gmail.com>
2017-12-11 00:33:15 -0600

    adding language examples

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,25 @@
 //= require turbolinks
 //= require_tree .
 
+
+var example_request = {
+
+    color_options: {
+        yellow: ["curl", "from", "import", "print", "require", "puts", "var", "function", "console"],
+        green: ["=", "log"],
+        grey: ["(", "{", "'", ")", "}", ",", ";"],
+        purple: ["'GET'", "'rest_client'", "'request'", "'Status:'", "'Headers:'", "'Response:'"]
+    },
+
+    cURL: 
+        "curl --include \\\ ~ 'https://olympicapi.herokuapp.com/api/' ~ ", 
+
+    Python: "from urllib2 import Request , urlopen ~ ~ request = Request ( 'https://olympicapi.herokuapp.com/api/' ) ~ ~ response_body = urlopen ( request ) .read ( ) ~ print response_body ~ ",
+
+    Ruby: "require 'rest_client' ~ ~ response = RestClient.get 'https://olympicapi.herokuapp.com/api/' ~ puts response ~ ",
+    Node: "var request = require ( 'request' ) ; ~ ~ request ( 'https://olympicapi.herokuapp.com/api/' , function ( error , response , body ) { ~ console.log ( 'Status:' , response.statusCode ) ; ~ console.log ( 'Headers:' , JSON.stringify ( response.headers ) ) ; ~ console.log ( 'Response:' , body ) ; ~ } ) ; ~"
+}
+
 $(document).ready(function() {
     listeners();
 });
@@ -20,11 +39,121 @@ let listeners = function() {
     });
 
 
-    $('.dark-dropdowns a').on('click', function(e) {
+    $('.dark-language-menu a').on('click', function(e) {
         e.preventDefault();
         $(this).parent().siblings('.dark-dropdowns-button').text($(this).text());
         $(this).parent().siblings('.dark-dropdowns-button').val($(this).text());
-        // console.log($('.root-url').html() + $('.dark-input-url').val());
+
+        dropdown_selection = $(this).parent().siblings('.dark-dropdowns-button').val();
+
+        $('.dark-language-div').html('');
+        $('.dark-line-count').html('');
+        
+        count = 1
+        depth = 0;
+        first_char = false;
+
+        $('.dark-language-div').append(`<span id=language-line${count} class='code-line'></span>`);
+        example_request[dropdown_selection].split(' ').forEach(function(item, index) {
+
+            next_grey = example_request.color_options.grey.indexOf(example_request[dropdown_selection].split(" ")[index + 1]);
+
+            if (item === "}" && first_char) {
+                depth -= 1;
+            }
+
+            if (depth > 0 && first_char) {
+                first_char = false
+                for(i = 0; i < depth; i++) {
+                    $(`#language-line${count}`).append("&emsp;&emsp;");
+                }
+            }
+
+
+            if (example_request.color_options.yellow.indexOf(item) !== -1 ) {
+
+                $(`#language-line${count}`).append("<span class='qmethod'>" + item + "</span>");
+                if (next_grey !== -1) {
+
+                } else {
+                    $(`#language-line${count}`).append("   ");
+                }
+
+            } else if (example_request.color_options.green.indexOf(item) !== -1) {
+
+                $(`#language-line${count}`).append("<span class='qgreen'>" + item + "</span>");
+                if (next_grey !== -1) {
+
+                } else {
+                    $(`#language-line${count}`).append("   ");
+                }
+
+            } else if (example_request.color_options.grey.indexOf(item) !== -1) {
+
+                $(`#language-line${count}`).append("<span style='color:grey'>" + item + "</span>");
+
+                if (item === ",") {
+                    $(`#language-line${count}`).append(" ");
+                }
+
+            } else if (example_request.color_options.purple.indexOf(item) !== -1) {
+
+                $(`#language-line${count}`).append("<span class='chrome-purple'>" + item + "</span>");
+
+                if (next_grey !== -1) {
+
+                } else {
+                    $(`#language-line${count}`).append("   ");
+                }
+
+            } else if (item === "~") {
+
+                first_char = true;
+                $(`#language-line${count}`).append("</br>");
+                if (count < 10) {
+                    $('.dark-line-count').append("<span class='text-muted'>" + "0" + (count) + "</span>" + "</br>");
+                } else {
+                    $('.dark-line-count').append("<span class='text-muted'>" + (count) + "</span>" + "</br>");
+                }
+
+                if ($(`#language-line${count}`).text().split('').length > 150) {
+                    $('.dark-line-count').append("</br></br>");
+                } else if ($(`#language-line${count}`).text().split('').length > 75) {
+                    $('.dark-line-count').append("</br>");
+                }
+
+                count += 1
+
+                $('.dark-language-div').append(`<span id=language-line${count} class='code-line'></span>`);
+
+
+            } else {
+
+                if (item === "console.log") {
+                    cl = item.split('.')
+                    $(`#language-line${count}`).append("<span class='qmethod'>" + cl[0] + "</span>");
+                    $(`#language-line${count}`).append("<span class='dark-code-font'>" + "." + "</span>");
+                    $(`#language-line${count}`).append("<span class='qgreen'>" + cl[1] + "</span>");
+                } else {
+
+                    $(`#language-line${count}`).append("<span class='dark-code-font'>" + item + "</span>");
+                }
+
+
+                if (next_grey !== -1) {
+
+                } else {
+                    $(`#language-line${count}`).append("   ");
+                }
+
+            }
+
+            if (item === "{" && first_char) {
+                depth += 1;
+            } 
+            
+        });
+
     });
 
     
