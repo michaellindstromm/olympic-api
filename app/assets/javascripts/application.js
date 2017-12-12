b9771d7a178b89f642671486713bd7616eae7dce
Michael Lindstrom <michaellindstr@gmail.com>
2017-12-12 15:09:48 -0600

    commenting applicationjs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,10 @@
 let request_uri = "'https://olympicapi.herokuapp.com/api/endpoints'"
 let base_uri = "'https://olympicapi.herokuapp.com/api/"
 let token;
+var selection;
 let count = 1
+
+// This object holds the example language requests that are formatted in the correct way. All characters and words are seperated by spaces, and line breaks are denoted by the '~' symbol. This object also holds the color options to colorized the out put correctly.
 
 var example_request = {
 
@@ -35,7 +38,7 @@ var example_request = {
     cSharp: `using System ; ~ ~ using System.Net.Http ; ~ ~ var baseAddress = new Uri ( [[URI_HOLDER]] ) ; ~ using ( var httpClient = new HttpClient { BaseAddress = baseAddress } ) { ~ ~ using ( var response = await httpClient.GetAsync ( 'undefined' ) ) { ~ ~ string responseData = await response.Content.ReadAsStringAsync() ; ~ ~ } ~ } ; ~`
 }
 
-var selection;
+// Have to use on 'turbolinks:load' instead of on 'ready', otherwise same page links will not load properly and the try server will not work when leaving and returning to index page without refresh. This calls the method to set the listeners and also to ready the try server.
 
 $(document).on('turbolinks:load', function () {
     if (window.location.pathname === '/') {
@@ -47,8 +50,10 @@ $(document).on('turbolinks:load', function () {
     }
 });
 
+// Listener specifically for the form, as to not load all home page listeners. These probably need to be allocated to their own file. For the future!
 function formListeners() {
 
+    // Disables button only when passwords match so form can only be submitted with correct password because bootstraps for validator does the rest for the other fields.
     $('#passwordConfirm').on('keyup', function(e) {
         if ($(this).val().length > 7 && $(this).val() === $('#password').val()) {
             $('.firstTokenButton').attr('disabled', false);
@@ -68,19 +73,24 @@ function formListeners() {
 };
 
 
+// Listeners for index page
 let homeListeners = function() {
 
+    // This prevents turbolinks get requests on any link click.
     $(document).on('turbolinks:click', function (event) {
         if (event.target.getAttribute('href').charAt(0) === '#') {
             return event.preventDefault()
         }
     })
 
+    // Scrollspy for sidebar
     $('body').scrollspy({ target: '#sidebar' })
     
+    // Listener for the try server language example dropdown
     $('.dark-language-menu a').on('click', function(e) {
         e.preventDefault();
 
+        // Sets the language selected to show for dropdown
         $(this).parent().siblings('.dark-dropdowns-button').text($(this).text());
         if ($(this).text() === "c#") {
             $(this).parent().siblings('.dark-dropdowns-button').val('cSharp');
@@ -92,11 +102,12 @@ let homeListeners = function() {
 
         selection = $(this).parent().siblings('.dark-dropdowns-button').val();
 
-        
-        format_as_json(example_request[selection], '.dark-language-line-count', '.dark-language-div');
+        // Calls method to take correct string from the example_request object and format it correctly.
+        format_as_json(example_request[selection], '.dark-language-div');
 
     });
 
+    // Listens for enter key to make call to requested endpoint and also to show what user is typing in request language div
     $('.dark-input-url').on('keyup', function(e) {
 
         if (e.which === 13) {
@@ -105,7 +116,7 @@ let homeListeners = function() {
 
         if (selection) {
 
-            format_as_json(example_request[selection], '.dark-language-line-count', '.dark-language-div')
+            format_as_json(example_request[selection], '.dark-language-div')
 
         }
     });
@@ -113,15 +124,23 @@ let homeListeners = function() {
     
 };
 
-function format_as_json(selection, counter_div, output_div) {
+// Method that formats and colorizes language request
+function format_as_json(selection, output_div) {
+
+    // clears output
     $(output_div).html('');
-    $(counter_div).html('');
+    // updates request endpoint to what user has inputed
     update_uri();
+
+    // variables used for formatting
     depth = 0;
     first_char = false;
     line_count = 1;
 
+    // create new line
     $(output_div).append(`<span id=language-line${count} class='code-line'></span>`);
+
+    // take correct language string, split it, and loop over each character
     selection.split(' ').forEach(function (item, index) {
 
         next_grey = example_request.color_options.grey.indexOf(selection.split(" ")[index + 1]);
@@ -221,10 +240,13 @@ function format_as_json(selection, counter_div, output_div) {
     });
 };
 
+
+// updates the request url to the one the user has inputed. used for display in language request div
 function update_uri() {
     request_uri = base_uri + $('.dark-input-url').val() + "'";
 }
 
+// makes call based on user input
 function makeTheCall() {
     $.ajax({
         url: "/api/" + $('.dark-input-url').val(),
@@ -235,6 +257,7 @@ function makeTheCall() {
     }).done(function (res) {
         seeResponse(res);
     }).fail(function(e) {
+        // checks to see if unauthorized meaning the token as expired and then makes the call again
         if (e.status === 401) {
             getNewToken();
         } else if (e.status === 404) {
@@ -247,6 +270,7 @@ function makeTheCall() {
     });
 }
 
+// gets a new token for use in the try server
 function getNewToken() {
     $.ajax({
         method: 'POST',
@@ -261,6 +285,7 @@ function getNewToken() {
     });
 }
 
+// called after a successful response is returned which stringifies the json, formats it, and then appends it to the response div for display
 function seeResponse(response) {
 
     response_as_string = JSON.stringify(response, null, 4)
@@ -270,6 +295,8 @@ function seeResponse(response) {
     $('.dark-response-div pre').append(syntaxHighlight(response_as_string, false));
 }
 
+
+// regex used for knowing which color to make json so it looks all pretty
 function syntaxHighlight(json) {
     json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
